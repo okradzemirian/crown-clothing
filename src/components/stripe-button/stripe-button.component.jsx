70ee0f5e5e62b9301fa06ad9f7d72c4e28e8d09e
Mirian Okradze <okradzemirian@gmail.com>
2019-10-22 17:28:29 +0400

    updating stripe integration and updating favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import StripeCheckout from 'react-stripe-checkout'
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100
+
+    const onToken = token => {
+        alert(token)
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='Crown Clothing Ltd'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}
+        />
+    )
+}
+
+export default StripeCheckoutButton
