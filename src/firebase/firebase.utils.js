30e012719696f6f8c0c79a4beb7aa1cd5a3b24ca
Mirian Okradze <okradzemirian@gmail.com>
2019-10-23 18:01:23 +0400

    implemented code that fetches shop data from firestore and stores it in the redux store, also included with-spinner HOC

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -45,6 +45,36 @@ export const createUserProfileDocument = async (
 firebase.initializeApp(firebaseConfig)
 firebase.analytics()
 
+export const addCollectionAndItems = async (collectionKey, objectsToAdd) => {
+    const collectionRef = firestore.collection(collectionKey)
+    const batch = firestore.batch()
+
+    objectsToAdd.forEach(obj => {
+        const newDocRef = collectionRef.doc()
+        batch.set(newDocRef, obj)
+    })
+
+    return await batch.commit()
+}
+
+export const convertCollectionsSnapshotToMap = collections => {
+    const transformedCollection = collections.docs.map(doc => {
+        const { title, items } = doc.data()
+
+        return {
+            id: doc.id,
+            title,
+            items,
+            routeName: encodeURI(title.toLowerCase()),
+        }
+    })
+
+    return transformedCollection.reduce((accumulator, collection) => {
+        accumulator[collection.title.toLowerCase()] = collection
+        return accumulator
+    }, {})
+}
+
 export const auth = firebase.auth()
 export const firestore = firebase.firestore()
 
