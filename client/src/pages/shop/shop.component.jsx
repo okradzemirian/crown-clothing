95c315bd94757d7e5141134c3e528cfd47070cc4
Mirian Okradze <okradzemirian@gmail.com>
2020-05-07 18:05:43 +0400

    added redux-saga and configured, moved collections fetching logic to sagas

--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -2,14 +2,14 @@ import React, { useEffect } from 'react'
 import { Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 
-import { fetchCollectionsStartAsync } from '../../redux/shop/shop-actions'
+import { fetchCollectionsStart } from '../../redux/shop/shop-actions'
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container'
 import CollectionPageContainer from '../collection/collection.container'
 
-const ShopPage = ({ match, fetchCollectionsStartAsync }) => {
+const ShopPage = ({ match, fetchCollectionsStart }) => {
     useEffect(() => {
-        fetchCollectionsStartAsync()
-    }, [fetchCollectionsStartAsync])
+        fetchCollectionsStart()
+    }, [fetchCollectionsStart])
 
     return (
         <div className='shop-page'>
@@ -27,10 +27,7 @@ const ShopPage = ({ match, fetchCollectionsStartAsync }) => {
 }
 
 const mapDispatchToProps = dispatch => ({
-    fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync()),
+    fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
 })
 
-export default connect(
-    null,
-    mapDispatchToProps,
-)(ShopPage)
+export default connect(null, mapDispatchToProps)(ShopPage)
